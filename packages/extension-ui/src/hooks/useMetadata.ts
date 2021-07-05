b0ee84216a6ab94d74c9a385a225e03c2a5b7079
KarishmaBothara <bothara.karishma@gmail.com>
2021-07-05 17:52:50 +1200

    fix nit

--- packages/extension-ui/src/hooks/useMetadata.ts
+++ packages/extension-ui/src/hooks/useMetadata.ts
@@ -6,7 +6,7 @@ import type { Chain } from '@polkadot/extension-chains/types';
 import { useEffect, useState } from 'react';
 
 import { getMetadata } from '../messaging';
-import BN from "bn.js";
+import BN from 'bn.js';
 
 export default function useMetadata (genesisHash?: string | null, specVersion?: BN | null, isPartial?: boolean): Chain | null {
   const [chain, setChain] = useState<Chain | null>(null);
